0dfae45b2cdf7b98f0d699a24d98c23f25beae7a
Dipayan Sukul <dipayansukul@hotmail.com>
2021-08-01 00:28:12 +0530

    fix(navitem.tsx): styled active link
    Added styling to active nav item in side bar

--- components/navitem.tsx
+++ components/navitem.tsx
@@ -3,8 +3,10 @@ import { ISidebarProps } from '../models/ISidebarProps';
 import { NavitemData } from '../data/navitemData';
 import { INavItem } from '../models/INavItem.interface';
 import { IconComponent } from './icon';
+import { useRouter } from 'next/router';
 
 export const NavItems: React.FC<ISidebarProps> = ({ toggle, setToggle }): JSX.Element => {
+	const router = useRouter();
 	const navItems: INavItem[] = NavitemData;
 	const elements =
 		navItems &&
@@ -21,7 +23,9 @@ export const NavItems: React.FC<ISidebarProps> = ({ toggle, setToggle }): JSX.El
 					<Link href={navItem.url} key={navItem.name}>
 						<a
 							onClick={() => setToggle(!toggle)}
-							className='flex w-full items-center text-white opacity-60 border-b border-white px-10 p-4 text-sm hover:opacity-100 transition duration-200'>
+							className={`flex w-full items-center text-white opacity-60 border-b border-white px-10 p-4 text-sm hover:opacity-100 transition duration-200 ${
+								router.pathname === navItem.url ? 'focus:opacity-100 font-extrabold' : ''
+							}`}>
 							<IconComponent name={navItem.name} className='w-5 h-5 mr-2' /> <span className='uppercase'>{navItem.name}</span>
 						</a>
 					</Link>
